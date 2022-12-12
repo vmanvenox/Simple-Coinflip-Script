b943d2e74f0c57e8822d3ca3e03a8c023330e6f3
Treemox <119058960+KingTreemox@users.noreply.github.com>
2022-12-12 14:58:06 +0100

    Add files via upload

---
+++ coinflip.py
@@ -0,0 +1,12 @@
+import random
+
+# Define the possible choices (heads and tails)
+choices = ["heads", "tails"]
+
+# Use the random.choice() function to choose one of the possible choices
+choice = random.choice(choices)
+
+# Print the result to the screen
+print(choice)
+
+# made by KingTreemox
